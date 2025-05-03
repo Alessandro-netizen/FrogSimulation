602df083ea38d9acea7cfc4d991dd61c8cbd6959
Alessandro-netizen <alessandrom12@nycstudents.net>
2025-05-03 11:12:25 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Test the simulate method (previous test)
+        
         FrogSimulation sim1 = new FrogSimulation(24, 5);
         System.out.println("--- Simulate Tests ---");
         System.out.println("Simulation 1 successful? " + sim1.simulate());
@@ -9,7 +9,7 @@ public class Main {
         System.out.println("Simulation 4 successful? " + sim1.simulate());
         System.out.println("Simulation 5 successful? " + sim1.simulate());
 
-        // Test the runSimulations method
+       
         FrogSimulation sim2 = new FrogSimulation(24, 5);
         int numSimulations = 4;
         System.out.println("\n--- Run Simulations Test ---");
